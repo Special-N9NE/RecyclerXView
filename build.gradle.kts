a0d6a505bcfdf2865677e3fd4ee6579c69cba22e
Special-N9NE <abigdeli42@gmail.com>
2023-08-09 00:45:34 +0330

    gradle changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,38 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("com.android.library") version "8.1.0" apply false
+    java
+    kotlin("jvm") version "1.6.0"
+    `maven-publish`
+}
+allprojects{
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+}
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("junit", "junit", "4.12")
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "Special-N9NE"
+            artifactId = "RecyclerXView"
+            version = "0.1.1"
+
+            from(components["java"])
+        }
+    }
 }
